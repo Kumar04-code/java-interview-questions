07038189321613cb6e653febe7abd959f50bd3e9
Raushan kumar <86177745+Kumar04-code@users.noreply.github.com>
2022-03-05 11:44:18 +0530

    Added Some Literal Examples

--- core-java/basics/Number.java
+++ core-java/basics/Number.java
@@ -13,5 +13,26 @@ public class Number {
 		n++;
 		System.out.println("Sum: "+ n);
 		
+		byte n2 = 0126;
+		
+		System.out.println("Sum: "+ n2);
+		n2++;
+		System.out.println("Sum: "+ n2);
+		n2++;
+		System.out.println("Sum: "+ n2);
+		n2++;
+		System.out.println("Sum: "+ n2);
+		
+		byte n3 = x0126;
+		
+		System.out.println("Sum: "+ n3);
+		n3++;
+		System.out.println("Sum: "+ n3);
+		n3++;
+		System.out.println("Sum: "+ n3);
+		n3++;
+		System.out.println("Sum: "+ n3);
+		
+		
 	}
 }
